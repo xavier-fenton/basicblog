26f64bc7409d333f06e5815da0b2b32f8990a2e1
Xavier Fenton <xavierfenton99@gmail.com>
2024-07-22 17:04:20 +1200

    Todos: Add comment

--- reactclient/src/Client.ts
+++ reactclient/src/Client.ts
@@ -1,4 +1,5 @@
 // Create fetch to .netcore database
+// Todo: Fetch all from DB and display in blogs component
 
 export const fetchPosts = fetch('http://localhost:5169/postItems', {method: 'GET'}).then((res) => {
     return res.body
