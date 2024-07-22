f0aac400e730a6daab19c44f51db159a81bbfcbc
Xavier Fenton <xavierfenton99@gmail.com>
2024-07-23 10:30:26 +1200

    update: naming file

--- reactclient/src/components/Blog.tsx
+++ reactclient/src/components/Blog.tsx
@@ -3,7 +3,7 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { fetchPosts } from '../Client'
+import { fetchPosts } from '../netclient'
 import { BlogPost } from '../types';
 
 
